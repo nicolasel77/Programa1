0884c024870464322df8e528a8a809c5d96fe92d
nicolasel77 <nicolasel77@gmail.com>
2020-02-04 14:41:44 -0300

    4-2

--- Programa1/Mail/frmMail.cs
+++ Programa1/Mail/frmMail.cs
@@ -1,6 +1,6 @@
-﻿using System;
+﻿using Microsoft.Office.Interop.Outlook;
+using System;
 using System.Windows.Forms;
-using Microsoft.Office.Interop.Outlook;
 
 namespace Programa1.Mail
 {
@@ -20,17 +20,30 @@ namespace Programa1.Mail
                 NameSpace outlookNamespace = outLookApp.GetNamespace("MAPI");
                 MAPIFolder inbox = outlookNamespace.GetDefaultFolder(OlDefaultFolders.olFolderInbox);
                 Items mailsItems = inbox.Items;
-                
+
                 mailsItems.Sort("[ReceivedTime]", true);
                 int n = 0;
                 label1.Text = "";
                 foreach (MailItem item in mailsItems)
                 {
-                    label1.Text = $"{label1.Text}\r{item.ReceivedTime.ToString()}  :  {item.Subject}";
                     if (item.Subject == "BALANZA")
                     {
-                        label1.Text = $"{label1.Text}\r        {item.Body}";
+                        label1.Text = $"{label1.Text}\r{item.ReceivedTime.ToString()}  :  {item.Subject}";
+
+                        //1) Fecha y Hora
+                        int fe = item.Body.IndexOf("Fecha:");
+                        if (fe != 0)
+                        {
+                            
+                            //string ss = item.Body.Substring(fe + 6, 16);
+                            //DateTime fecha;
+
+                        }
+
+
+                        //TODO: Responder para confirmar operación.
                     }
+
                     if (n == 10)
                     {
                         break;
@@ -47,6 +60,8 @@ namespace Programa1.Mail
             {
                 label1.Text = $"{label1.Text}\rException caught: {ex.Message}";
             }
-        }        
+        }
+
+
     }
 }
